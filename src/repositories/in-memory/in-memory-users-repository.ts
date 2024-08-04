94a0c735efc7f38deddf213ad9bb8252bdaef8f2
ptkrnnan <ptk.rnnan02@gmail.com>
2024-08-04 03:20:55 -0300

    create check in

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
@@ -27,7 +28,7 @@ export class InMemoryUsersRepository implements UsersRepository {
   // Criando um banco 'em memoria' para usar em test unitarios, que nao precisam acessar o banco de dados
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
